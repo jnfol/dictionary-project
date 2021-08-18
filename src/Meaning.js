d193030db50cf35a2b9b104599c10460ce022e7f
jnfol <jessi.follett@gmail.com>
2021-08-18 17:33:03 -0600

    Created synonyms function to display synonyms of words when available

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -12,6 +13,8 @@ export default function Meaning(props) {
               {definition.definition}
               <br />
               <em> {definition.example} </em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
